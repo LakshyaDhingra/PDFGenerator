126cf7018e49f0e55b9fb1222aa93a2a328a1cd0
Lakshya Dhingra <dlakshya223@gmail.com>
2024-06-23 11:47:20 +0530

    Topic insertion in PDF

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+df = pd.read_csv("topics.csv")
 
-pdf.set_font(family="Times", style="B", size=12)
-
-pdf.cell(w=0, h=12, txt="Hi! My Name Is", align="L", ln=1, border=1)
+for index, row in df.iterrows():
+    topic = row["Topic"]
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=topic, align="L", ln=1)
+    pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
